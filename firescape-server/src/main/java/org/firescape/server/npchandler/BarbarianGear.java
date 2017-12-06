bc9aa94de080ca80745f22807508a16e19ba0860
ahhh <ahhh.db@gmail.com>
2017-12-06 00:00:33 -0800

    adding 3 npc dialogs, priest, dwarf, and barbarian

---
+++ firescape-server/src/main/java/org/firescape/server/npchandler/BarbarianGear.java
@@ -0,0 +1,80 @@
+package org.firescape.server.npchandler;
+
+import org.firescape.server.event.ShortEvent;
+import org.firescape.server.model.*;
+
+public class BarbarianGear implements NpcHandler {
+  /**
+   * World instance
+   */
+  public static final World world = World.getWorld();
+
+  public void handleNpc(final Npc npc, final Player player) throws Exception {
+    player.informOfNpcMessage(new ChatMessage(npc, "Hey pal, I sell meats and axs, 20gp each", player));
+    player.setBusy(true);
+    world.getDelayedEventHandler().add(new ShortEvent(player) {
+      public void action() {
+        owner.setBusy(false);
+        String[] options = new String[]{
+                "No thank you",
+                "Sure, I'll take an axe!",
+                "Sure, I'll take some meat!"
+        };
+        owner.setMenuHandler(new MenuHandler(options) {
+          public void handleReply(final int option, final String reply) {
+            if (owner.isBusy()) {
+              return;
+            }
+            owner.informOfChatMessage(new ChatMessage(owner, reply, npc));
+            owner.setBusy(true);
+            world.getDelayedEventHandler().add(new ShortEvent(owner) {
+              public void action() {
+                owner.setBusy(false);
+                if (option == 1) {
+                  if (owner.getInventory().remove(10, 20) > -1) {
+                    owner.getActionSender().sendMessage("You buy a steel axe");
+                    owner.getInventory().add(new InvItem(89, 1));
+                    owner.getActionSender().sendInventory();
+                    npc.unblock();
+                  } else {
+                    owner.informOfChatMessage(new ChatMessage(owner, "Get out of here!", npc));
+                    owner.setBusy(true);
+                    world.getDelayedEventHandler().add(new ShortEvent(owner) {
+                      public void action() {
+                        owner.setBusy(false);
+                        owner.informOfNpcMessage(new ChatMessage(npc, "See ya", owner));
+                        npc.unblock();
+                      }
+                    });
+                  }
+                } else if (option == 2) {
+                  if (owner.getInventory().remove(10, 20) > -1) {
+                    owner.getActionSender().sendMessage("You buy a tasty meat pie");
+                    owner.getInventory().add(new InvItem(259, 1));
+                    owner.getActionSender().sendInventory();
+                    npc.unblock();
+                  } else {
+                    owner.informOfChatMessage(new ChatMessage(owner, "Get out of here!", npc));
+                    owner.setBusy(true);
+                    world.getDelayedEventHandler().add(new ShortEvent(owner) {
+                      public void action() {
+                        owner.setBusy(false);
+                        owner.informOfNpcMessage(new ChatMessage(npc, "See ya", owner));
+                        npc.unblock();
+                      }
+                    });
+                  } 
+                } else {
+                  npc.unblock();
+                }
+              }
+            });
+          }
+        });
+        owner.getActionSender().sendMenu(options);
+      }
+    });
+    npc.blockedBy(player);
+  }
+
+}
